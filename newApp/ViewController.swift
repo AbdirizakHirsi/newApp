d8e74efc5719c4bbbd1fc75e38434d8c2d57f082
Abdirizak Hirsi <a.hirsi2@wlv.ac.uk>
2017-02-27 18:44:32 +0000

    Initial Commit

---
+++ newApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newApp
+//
+//  Created by Abdirizak Hirsi on 27/02/2017.
+//  Copyright © 2017 Abdirizak Hirsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
